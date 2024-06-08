69cd81e2713bef249afcd62a6b974cc9ca42e69b
jenitarianti <rizky_herdian_saputra9308@gmail.com>
2024-06-08 20:04:33 +0700

    baru

---
+++ Website UMKM Kobargrill (2)/Website UMKM Kobargrill/js/main.js
@@ -0,0 +1,156 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    function redirectToHref() {
+        // Redirect to the desired URL
+        window.location.href = "index.html";
+      }
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+
+document.addEventListener('DOMContentLoaded', function () {
+    var loginForm = document.querySelector('.login-form form');
+    var signupForm = document.querySelector('.signup-form form');
+
+    // Validasi form login
+    loginForm.addEventListener('submit', function (event) {
+        var emailInput = document.querySelector('.login-form input[type="text"]');
+        var passwordInput = document.querySelector('.login-form input[type="password"]');
+
+        if (!isValidEmail(emailInput.value)) {
+            alert('Masukkan email dengan benar!');
+            event.preventDefault();
+        }
+    });
+
+    // Validasi form signup
+    signupForm.addEventListener('submit', function (event) {
+        var emailInput = document.querySelector('.signup-form input[type="text"]');
+        var passwordInput = document.querySelector('.signup-form input[type="password"]');
+
+        if (!isValidEmail(emailInput.value)) {
+            alert('Masukkan email dengan benar!');
+            event.preventDefault();
+        }
+    });
+
+    // Fungsi validasi email
+    function isValidEmail(email) {
+        // Gunakan regular expression untuk memeriksa format email
+        var emailPattern = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+        return emailPattern.test(email);
+    }
+});
